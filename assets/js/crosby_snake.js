682d5472ac3079fbecd0d1c018396a5837a206b0
Tag <tagtribble@gmail.com>
2026-04-27 19:06:35 -0400

    Feat: Made the box for the game apear when play is clicked.

--- assets/js/crosby_snake.js
+++ assets/js/crosby_snake.js
@@ -1,2 +1,21 @@
-gameBox = document.getElementById('game_box');
+const gameBox = document.getElementById('game_box');
+const inputForm = document.getElementById('game_input');
+const submitBtn = document.getElementById('submit');
 
+function crosbySnake(food, size, speed){
+    document.getElementById('grey_out').style.display = 'block';
+    document.getElementById('crosby_game').style.display = 'block';
+}
+
+inputForm.addEventListener('submit', (e) => {
+    event.preventDefault();
+
+    try {
+        const choicesRaw = new FormData(e.target);
+        const choices = Object.fromEntries(choicesRaw.entries());
+        console.log(choices);
+        crosbySnake(choices.food, choices.size, choices.speed);
+    }catch(error) {
+        console.log(error);
+    }
+});
